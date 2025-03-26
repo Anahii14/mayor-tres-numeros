da1aefa03f1705a595ef1bff231c5a36aaec649e
tu_usuario <tucorreo@tuservicio.com>
2025-03-26 11:24:26 -0500

    Implementación de mayor de tres numeros

---
+++ mayor-tres-numeros.py
@@ -0,0 +1,25 @@
+"""
+Determina el mayor de tres numeros ingresados por el teclado
+"""
+numero1=int(input("Ingrese el primer numero: "))
+numero2=int(input("Ingrese el segundo numero: "))
+numero3=int(input("Ingrese el tercer numero: "))
+
+if numero1>numero2:
+    temporal=numero1 #temporal es
+    numero1=numero2
+    numero2=temporal
+
+if numero2>numero3:
+    temporal=numero2 #temporal es
+    numero2=numero3
+    numero3=temporal
+
+if numero1>numero2:
+    temporal=numero1 #temporal es
+    numero1=numero2
+    numero2=temporal
+
+print(f"numeros ordenados: {numero1}, {numero2},{numero3}")
+print(f"El mayor es {numero3}")
+
